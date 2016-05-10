89b29ee362e15a592db6db1c5593217e96e0cbba
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2016-05-10 15:17:31 -0300

    add injectable bootstrap

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -9,8 +9,10 @@ export class ProjectConfig extends SeedConfig {
     super();
     // this.APP_TITLE = 'Put name of your app here';
     let additional_deps: InjectableDependency[] = [
-      // {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
-      // {src: 'lodash/lodash.min.js', inject: 'libs'},
+      {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
+      {src: 'lodash/lodash.min.js', inject: 'libs'},
+      {src: 'bootstrap/dist/js/bootstrap.min.js', inject: 'libs'},
+      {src: 'bootstrap/dist/css/bootstrap.min.css', inject: true},
     ];
 
     const seedDependencies = this.NPM_DEPENDENCIES;
