f2cb6b7c7b4d9d74f81d9fa94457359016a3a60a
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2016-05-11 16:24:40 -0300

    add sidebar links

---
+++ src/app/components/sidebar/inbox.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'sidebar-inbox',
+  moduleId: module.id,
+  templateUrl: './inbox.component.html',
+  styleUrls: ['./inbox.component.css']
+})
+export class SidebarInboxComponent {}
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,9 +1,14 @@
 import {Component} from 'angular2/core';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
+
+// Componentes de Navbar
+import {SidebarInboxComponent} from './inbox.component';
 
 @Component({
   selector: 'sd-sidebar',
   moduleId: module.id,
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
+  directives: [ROUTER_DIRECTIVES, SidebarInboxComponent]
 })
 export class SidebarComponent {}
