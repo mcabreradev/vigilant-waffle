1d6ae55484dc79b1f8c882d97af22444cf4938f8
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2016-05-11 13:53:26 -0300

    add font task

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -5,6 +5,11 @@ import {InjectableDependency} from './seed.config.interfaces';
 export class ProjectConfig extends SeedConfig {
   PROJECT_TASKS_DIR = join(process.cwd(), this.TOOLS_DIR, 'tasks', 'project');
 
+  FONTS_DEST = `${this.APP_DEST}/fonts`;
+  FONTS_SRC = [
+      'node_modules/font-awesome/css/font-awesome.min.css',
+  ];
+
   constructor() {
     super();
     // this.APP_TITLE = 'Put name of your app here';
@@ -13,10 +18,12 @@ export class ProjectConfig extends SeedConfig {
       {src: 'lodash/lodash.min.js', inject: 'libs'},
       {src: 'bootstrap/dist/js/bootstrap.min.js', inject: 'libs'},
       {src: 'bootstrap/dist/css/bootstrap.min.css', inject: true},
+      {src: 'font-awesome/css/font-awesome.min.css', inject: true},
     ];
 
     const seedDependencies = this.NPM_DEPENDENCIES;
 
     this.NPM_DEPENDENCIES = seedDependencies.concat(additional_deps);
   }
+
 }
