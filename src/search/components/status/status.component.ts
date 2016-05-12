c95a00f442ec901071d65d9f09519556c5c8bb16
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2016-05-12 20:02:20 -0300

    add search with params

--- src/search/components/status/status.component.ts
+++ src/search/components/status/status.component.ts
@@ -1,8 +1,14 @@
-import {Component} from 'angular2/core';
+import { Component } from 'angular2/core';
+import { RouteParams } from 'angular2/router';
 
 @Component({
   moduleId: module.id,
   templateUrl: './status.component.html',
   styleUrls: ['./status.component.css']
 })
-export class StatusComponent {}
+export class StatusComponent {
+	status: string;
+    constructor(params: RouteParams) {
+        this.status = params.get('status');
+    }
+}
