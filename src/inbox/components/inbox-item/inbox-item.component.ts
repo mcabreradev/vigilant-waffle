34c471e6ff1765da87a6b16b9abd55ad97557f35
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2016-05-13 18:10:59 -0300

    inbox item

---
+++ src/inbox/components/inbox-item/inbox-item.component.ts
@@ -0,0 +1,9 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'inbox-item',
+  moduleId: module.id,
+  templateUrl: './inbox-item.component.html',
+  styleUrls: ['./inbox-item.component.css']
+})
+export class InboxItemComponent {}
--- src/inbox/components/inbox.component.ts
+++ src/inbox/components/inbox.component.ts
@@ -1,8 +1,10 @@
 import { Component } from 'angular2/core';
+import {InboxItemComponent} from './inbox-item/inbox-item.component';
 
 @Component({
   moduleId: module.id,
   templateUrl: './inbox.component.html',
-  styleUrls: ['./inbox.component.css']
+  styleUrls: ['./inbox.component.css'],
+  directives: [InboxItemComponent]
 })
 export class InboxComponent {}
