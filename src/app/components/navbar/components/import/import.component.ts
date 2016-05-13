d338b69792c3d0f4653f2f69d335436fd70b0ac8
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2016-05-13 10:53:53 -0300

    add -> import component

---
+++ src/app/components/navbar/components/import/import.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  moduleId: module.id,
+  templateUrl: './import.component.html',
+  styleUrls: ['./import.component.css']
+})
+export class ImportComponent {}
