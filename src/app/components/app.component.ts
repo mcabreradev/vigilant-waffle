fd79dc04b6d2a824e54edcd2ad540aa6ea51c2dc
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2016-05-13 12:36:02 -0300

    remove oportunities

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -10,7 +10,6 @@ import { HomeComponent } from '../../home/components/home.component';
 import { AboutComponent } from '../../about/components/about.component';
 import { InboxComponent } from '../../inbox/components/inbox.component';
 import { TasksComponent } from '../../tasks/components/tasks.component';
-import { OportunitiesComponent } from '../../oportunities/components/oportunities.component';
 import { ReportingComponent } from '../../reporting/components/reporting.component';
 import { ImportComponent } from '../../import/components/import.component';
 import { SettingsComponent } from '../../settings/components/settings.component';
@@ -38,7 +37,6 @@ import { NameListService } from '../../shared/services/name-list.service';
   { path: '/about', name: 'About', component: AboutComponent },
   { path: '/inbox', name: 'Inbox', component: InboxComponent },
   { path: '/tasks/:param', name: 'Tasks', component: TasksComponent }, // done future
-  { path: '/oportunities', name: 'Oportunities', component: OportunitiesComponent },
   { path: '/search', name: 'Leads', component: SearchComponent },
   { path: '/search/never-contacted', name: 'NeverContacted', component: NeverContactedComponent },
   { path: '/search/opened-email', name: 'OpenedEmail', component: OpenedEmailComponent },
--- src/oportunities/components/oportunities.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-  moduleId: module.id,
-  templateUrl: './oportunities.component.html',
-  styleUrls: ['./oportunities.component.css']
-})
-export class OportunitiesComponent {}
