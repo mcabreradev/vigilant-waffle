c32d5074f9f74203c7ea0a8785cdd5d60587b9ac
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2016-05-13 12:12:52 -0300

    relocate inbox component

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -8,9 +8,12 @@ import { SidebarComponent } from './sidebar/components/sidebar.component';
 // Isolated Components
 import { HomeComponent } from '../../home/components/home.component';
 import { AboutComponent } from '../../about/components/about.component';
+import { InboxComponent } from '../../inbox/components/inbox.component';
 import { TasksComponent } from '../../tasks/components/tasks.component';
 import { OportunitiesComponent } from '../../oportunities/components/oportunities.component';
 import { ReportingComponent } from '../../reporting/components/reporting.component';
+import { ImportComponent } from '../../import/components/import.component';
+import { SettingsComponent } from '../../settings/components/settings.component';
 
 // Search
 import { SearchComponent } from '../../search/components/search.component';
@@ -33,7 +36,8 @@ import { NameListService } from '../../shared/services/name-list.service';
 @RouteConfig([
   { path: '/',      name: 'Home',  component: HomeComponent, useAsDefault: true },
   { path: '/about', name: 'About', component: AboutComponent },
-  { path: '/tasks/:param', name: 'Tasks', component: TasksComponent }, // inbox done future
+  { path: '/inbox', name: 'Inbox', component: InboxComponent },
+  { path: '/tasks/:param', name: 'Tasks', component: TasksComponent }, // done future
   { path: '/oportunities', name: 'Oportunities', component: OportunitiesComponent },
   { path: '/search', name: 'Leads', component: SearchComponent },
   { path: '/search/never-contacted', name: 'NeverContacted', component: NeverContactedComponent },
@@ -41,5 +45,7 @@ import { NameListService } from '../../shared/services/name-list.service';
   { path: '/search/recently-created', name: 'RecentlyCreated', component: RecentlyCreatedComponent },
   { path: '/search/status/:status', name: 'Status', component: StatusComponent },
   { path: '/reporting', name: 'Reporting', component: ReportingComponent },
+  { path: '/import', name: 'Import', component: ImportComponent },
+  { path: '/settings', name: 'Settings', component: SettingsComponent },
 ])
 export class AppComponent {}
