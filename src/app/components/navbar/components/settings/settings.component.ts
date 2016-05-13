75b999e8a05805bc1689846b589686a757f58a0e
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2016-05-13 10:54:09 -0300

    add settings component

---
+++ src/app/components/navbar/components/settings/settings.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  moduleId: module.id,
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.css']
+})
+export class SettingsComponent {}
